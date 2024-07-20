16785ec08488eb9dfcf4e0adf47e2440335dadd2
vrbot584 <akashnayak77718@gmail.com>
2024-07-20 19:31:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 #Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7462081683:AAEBMRjFtQ1U6TMZMIpRA35tBkaW3JL2Sy4")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "29412591"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "bc5652168138eeaf562cac0752c4b48e")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://akashnayak77718:<KIgScEGXx4nt9Mmx>@cluster0.vwilgjo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
